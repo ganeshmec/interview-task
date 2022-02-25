dcae84838e21e06f917fb25ec4ed4286b4da66da
ganeshmec <bommireddy.ganesh@gmail.com>
2022-02-25 13:20:37 +0530

    task

--- src/app/kanbanBoard/kanbanBoard.component.ts
+++ src/app/kanbanBoard/kanbanBoard.component.ts
@@ -1,3 +1,4 @@
+
 import {Component, OnInit} from '@angular/core';
 
 @Component({
@@ -8,10 +9,12 @@ import {Component, OnInit} from '@angular/core';
 export class KanbanBoard implements OnInit {
   tasks: Task[];
   stagesNames: string[];
+  public newTask: string;
+  public taskLength: number;
   stagesTasks: any[]; //Only used for rendering purpose
 
   ngOnInit() {
-    // Each task is uniquely identified by its name. 
+    // Each task is uniquely identified by its name.
     // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
     this.tasks = [
       { name: '0', stage: 0 },
@@ -20,7 +23,17 @@ export class KanbanBoard implements OnInit {
     this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
     this.configureTasksForRendering();
   }
-  
+  createTask(){
+    if(!this.newTask){
+      return ;
+    }
+    this.tasks.push({
+      name: this.newTask, stage: 0
+    });
+    this.newTask = '';
+    this.configureTasksForRendering();
+  }
+
   // this function has to be called whenever tasks array is changed to construct stagesTasks for rendering purpose
   configureTasksForRendering = () => {
     this.stagesTasks = [];
@@ -31,6 +44,35 @@ export class KanbanBoard implements OnInit {
       const stageId = task.stage;
       this.stagesTasks[stageId].push(task);
     }
+    this.taskLength = this.tasks.length;
+  }
+  forwardMove(task){
+    this.tasks = this.tasks.map(i => {
+      if(i.name === task.name){
+        i.stage = i.stage + 1;
+      }
+      return i;
+    });
+    this.configureTasksForRendering();
+
+  }
+  backMove(task){
+    this.tasks = this.tasks.map(i => {
+      if(i.name === task.name){
+        i.stage = i.stage - 1;
+      }
+      return i;
+    });
+    this.configureTasksForRendering();
+
+  }
+  delete(task){
+    this.tasks = this.tasks.filter(i => {
+      if(i.name !== task.name){
+        return i
+      }
+    });
+    this.configureTasksForRendering();
   }
 
   generateTestId = (name) => {
